6a0a7eb91a7919df75d7f4a73f25f58c841c537b
Pranav More <pranavmore97@gmail.com>
2016-09-20 00:15:48 +1000

    use morgan logging

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express');  
 const request = require('request');
 const cors = require('cors');
-
+const morgan = require('morgan');
 const apiServerHost = process.argv[2];
 
 const app = express();
 app.use(cors());
-
+app.use(morgan('dev'));
 app.use('/', function(req, res) {  
   console.log(req.url);
   const url = apiServerHost + req.url;
