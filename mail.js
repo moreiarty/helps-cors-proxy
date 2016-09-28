48bdaeac4be53de7c88004a326e3b9bbe9b6d01c
Jason Shin <visualbbasic@gmail.com>
2016-09-28 22:24:03 +1000

    mail implemented, it's using simplesmtp. ask jason for config file

---
+++ mail.js
@@ -0,0 +1,47 @@
+/**
+ * Created by Shin on 28/09/2016.
+ */
+var config = require('./config/smtp');
+var simplesmtp = require('simplesmtp');
+
+mail('sender@example.com', 'visualbbasic@gmail.com', 'subject: test\r\n\r\nhello world!');
+
+/**
+ * Send a raw email
+ *
+ * @param {String} from E-mail address of the sender
+ * @param {String|Array} to E-mail address or a list of addresses of the receiver
+ * @param {[type]} message Mime message
+ */
+function mail(from, to, message) {
+    var client = simplesmtp.connect(465, 'smtp.gmail.com', {
+        secureConnection: true,
+        auth: config,
+        debug: true
+    });
+
+    client.once('idle', function() {
+        client.useEnvelope({
+            from: from,
+            to: [].concat(to || [])
+        });
+    });
+
+    client.on('message', function() {
+        client.write(message.replace(/\r?\n/g, '\r\n').replace(/^\./gm, '..'));
+        client.end();
+    });
+
+    client.on('ready', function(success) {
+        client.quit();
+    });
+
+    client.on('error', function(err) {
+        console.log('ERROR');
+        console.log(err);
+    });
+
+    client.on('end', function() {
+        console.log('DONE')
+    });
+}
