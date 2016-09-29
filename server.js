58ba23d5f861668b89c9806053c747ce70783f83
Jason Shin <visualbbasic@gmail.com>
2016-09-29 11:50:11 +1000

    Updated readme to correctly guide users on how to proxy

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const app = express();
 app.use(cors());
 app.use(morgan('dev'));
 app.use('/api/', function(req, res) {
+  console.log('routing correctly');
   console.log(req.url);
   const url = apiServerHost + req.url;
   req.pipe(request(url)).pipe(res);
