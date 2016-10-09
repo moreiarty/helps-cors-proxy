4b7111f97636137b2c5de1bc08a699a5a8acb3e7
Pranav More <pranavmore97@gmail.com>
2016-10-09 14:11:44 +1100

    Fix sms config reference

--- helpers/sms.helper.js
+++ helpers/sms.helper.js
@@ -1,8 +1,8 @@
 const Telstra = require('telstra-api');
 
 const configSms = require('../config/sms');
-const CONSUMER_KEY = config.key;
-const CONSUMER_SECRET = config.secret;
+const CONSUMER_KEY = configSms.key;
+const CONSUMER_SECRET = configSms.secret;
 const SCOPE = "SMS";
 
 const t = new Telstra(CONSUMER_KEY, CONSUMER_SECRET, SCOPE);
